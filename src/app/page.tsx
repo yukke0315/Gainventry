44064a10b28a4662cf030f5aa99950d88ed1ea37
yukke0315 <is0786rr@ed.ritsumei.ac.jp>
2025-06-04 21:49:57 +0900

    Use useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ExerciseForm from "@/components/ExerciseForm"
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">🏋️ Gainventory</h1>
+      <h1 className="text-4xl font-bold mb-4">🏋️ Gainventory</h1>
       <ExerciseForm />
     </main>
   )
--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -1,6 +1,41 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { supabase } from '@/lib/supabaseClient';
 
 const ExerciseForm = () => {
+    // 種目名用の箱
+    const [name, setName] = useState('');
+    // カテゴリー用の箱
+    const [category, setCategory] = useState('');
+    // 登録中か判別用の箱
+    const [loading, setLoading] = useState(false);
+
+    // 登録処理
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();   // リフレッシュ防止
+        setLoading(true);   // ローディング中
+
+    try {
+        const { error } = await supabase
+            .from('exercises')
+            .insert([{ name, category}]);
+
+        if (error) {
+            console.error('Supabase登録エラー', error);
+            throw error;
+        }
+
+        alert('種目を登録しました！');
+        setName('');   // 登録終了後、箱を空に
+        setCategory('');
+    } catch (error: any) {
+        alert('登録に失敗しました' + error.message);
+    } finally {
+        setLoading(false);   // 登録終わり
+    }
+    };
+
     return (
         <form>
             <h2>新しい種目を追加</h2>
@@ -9,7 +44,10 @@ const ExerciseForm = () => {
                 <input
                     type="text"
                     id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     className="mt-1 block w-full border border-gray-300 shadow-sm rounded-md p-2"
+                    required
                 />                
             </div>
             <div>
@@ -17,15 +55,19 @@ const ExerciseForm = () => {
                 <input
                     type="text"
                     id="category"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
                     className="mt-1 block w-full border border-gray-300 shadow-sm rounded-md p-2"
                     placeholder="例：胸, 腕, 脚"
+                    required
                 />    
             </div>
             <button
                 type="submit"
+                disabled={loading}
                 className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
             >
-                登録
+                {loading ? '登録中...' : '登録'}
             </button>    
         </form>
     );
